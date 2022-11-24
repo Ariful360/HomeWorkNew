c673a35ffd4055da8ffd942f19231a30f20a8db7
Ariful360 <arfulislmqa@gmail.com>
2022-11-23 22:15:34 -0500

    all hw

---
+++ src/hw1JavaBasicold/MySelf.java
@@ -0,0 +1,19 @@
+package hw1JavaBasicold;
+/*
+ * Project start with upper case
+ * Package start with lower case
+ * class start with upper case
+ */
+
+public class MySelf {
+	// Entry point of java
+	// Main is the actual method
+
+	public static void main(String[] args) {
+		System.out.println("Ariful Islam");
+		System.out.println("61-07 Woodside ave Woodside, NY 11377");
+		System.out.println("\"EnthirlIt\"");
+
+	}
+
+}
