bf1b6bb6283c499a0eb35056b37515e7396b7760
Ariful360 <arfulislmqa@gmail.com>
2022-11-23 22:08:09 -0500

    hw2

---
+++ src/hw2JavaBasics/MyCar.java
@@ -0,0 +1,15 @@
+package hw2JavaBasics;
+
+public class MyCar {
+	public static void main(String[] args) {
+		String name = "Ariful";
+
+		System.out.println ("\tMy name is \"Ariful\"," + "\nI love to drive My Car");
+
+		System.out.print("model: Tlx");
+		System.out.print("Color: White");
+		System.out.print("Year: 2015");
+		System.out.print("Wheel Drive: FWD");
+	}
+
+}
